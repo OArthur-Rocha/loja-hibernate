7c8d8f24de8c79fe79fa647a88e5f1c992927290
Arthur Rocha <arthur.oliveira182@hotmail.com>
2023-06-27 22:52:31 -0300

    Primeiro commit. Estudos JPA

---
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,29 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.dao.CategoriaDAO;
+import br.com.alura.loja.dao.ProdutoDAO;
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+
+public class CadastroDeProduto {
+
+    public static void main(String[] args) {
+        Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("RedMi Note 12", "Muito legal", new BigDecimal("800"),
+                celulares);
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+
+        em.getTransaction().begin();
+        categoriaDAO.cadastrar(celulares);
+        produtoDAO.cadastrar(celular);
+        em.getTransaction().commit();
+        em.close();
+    }
+}
